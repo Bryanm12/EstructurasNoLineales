e2b66de792e00047afbd5f624f230b9fa02649b2
david <davidsanchez363@gmail.com>
2024-06-20 16:16:27 -0400

    Estructuras no lineales

---
+++ src/main/App.java
@@ -0,0 +1,40 @@
+package main;
+
+import main.Materia.Controllers.ArbolBinario;
+import main.Materia.Models.Node;
+/*
+ *            1
+ *        2        3
+ *     4     5       6
+ *    7     8
+ */
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+                ArbolBinario arbolBinario = new ArbolBinario();
+        Node nodeRoot = new Node(1);
+        nodeRoot.setLeft(new  Node(2));
+        nodeRoot.setRight(new Node(3));
+        
+        nodeRoot.getLeft().setLeft(new Node(4));
+        nodeRoot.getLeft().setRight(new Node(5));
+
+        nodeRoot.getRight().setRight(new Node(6));
+
+        nodeRoot.getLeft().getLeft().setLeft(new Node(7));
+        nodeRoot.getLeft().getRight().setLeft(new Node(8));
+
+        System.out.println("PreOrder - Loop");
+        arbolBinario.preOrderLoop(nodeRoot);
+        System.out.println();
+        System.out.println("\nPreOrder - Recursivo");
+        arbolBinario.preOrderRecursivo(nodeRoot);
+        System.out.println("\nPostOrder - Loop");
+        arbolBinario.postOrderLoop(nodeRoot);
+        System.out.println("\nPostOrder - Recursivo");
+        arbolBinario.postOrderRecursivo(nodeRoot);
+        System.out.println("\n InOrder - Recursivo");
+        arbolBinario.inOrderRecursivo(nodeRoot);
+    }
+}
---
+++ src/main/Materia/Controllers/ArbolBinario.java
@@ -0,0 +1,83 @@
+package main.Materia.Controllers;
+import java.util.Stack;
+
+import main.Materia.Models.Node;
+
+public class ArbolBinario {
+    
+        //Recorrido del arbol de tipo PreOrder
+    public void preOrderLoop(Node root){
+        if(root == null){
+            return;
+        }
+
+        Stack<Node> stack = new Stack<>();
+        //Agregamos a la pila la raiz para empezar el recorrido
+        stack.push(root);
+        while(!stack.isEmpty()){
+            Node node = stack.pop();
+
+            System.out.print(node.getValue()+" - ");
+            //Agregar los hijos de la derecha a la pila
+            if(node.getRight() != null){
+                stack.push(node.getRight());
+            }
+            //Agregar los hijos de la izquierda a la pila
+            if(node.getLeft() != null ){
+                stack.push(node.getLeft());
+            }
+        }
+
+
+    }
+
+    public void preOrderRecursivo(Node node){
+        if(node != null){
+            System.out.print(node.getValue()+" - ");
+            preOrderRecursivo(node.getLeft());
+            preOrderRecursivo(node.getRight());
+        }
+
+    }
+    
+    
+    public void postOrderLoop(Node root){
+
+        if(root == null){
+            return;
+        }
+        Stack<Node> stack1 = new Stack<>();
+        Stack<Node> stack2 = new Stack<>();
+        stack1.push(root);
+
+        while(!stack1.isEmpty()){
+            Node node = stack1.pop();
+            stack2.push(node);
+
+            if(node.getLeft()!=null){
+                stack1.push(node.getLeft());
+            }
+            if(node.getRight()!=null){
+                stack1.push(node.getRight());
+            }
+        }
+        while(!stack2.isEmpty()){
+            Node node = stack2.pop();
+            System.out.print(node.getValue()+" ");
+        }
+
+
+    }
+    public void postOrderRecursivo(Node node){
+        if(node!=null){
+            postOrderRecursivo(node.getLeft());
+            postOrderRecursivo(node.getRight());
+            System.out.print(node.getValue()+" ");
+        }
+    }
+    public void inOrderRecursivo(Node node){
+        postOrderRecursivo(node.getLeft());
+        System.out.print(node.getValue()+" ");
+        postOrderRecursivo(node.getRight());
+    }
+}
