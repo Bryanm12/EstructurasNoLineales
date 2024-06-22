8fa91c597af4ecbc103e6d8cca40ff891786bccf
Bryan <bryanm31421@gmail.com>
2024-06-21 20:37:19 -0500

    Estructuras no Lineales2

--- src/main/Ejercicio_03_listLeves/ListLevels.java
+++ src/main/Ejercicio_03_listLeves/ListLevels.java
@@ -1,11 +1,27 @@
 package main.Ejercicio_03_listLeves;
 
+import java.util.ArrayList;
 import java.util.List;
+
+
 
 import main.Materia.Models.Node;
 
 public class ListLevels {
     public List<List<Node>> listLevels(Node root){
-        return null;
+        List<List<Node>> resultado = new ArrayList<>();
+        levelListsHelper(root, 0, resultado);
+        return resultado;
+    }
+
+    private void levelListsHelper(Node node, int nivel, List<List<Node>> resultado) {
+        if (node == null)
+        return;      
+        if (nivel == resultado.size()) {
+            resultado.add(new ArrayList<>());
+        }
+        resultado.get(nivel).add(node);
+        levelListsHelper(node.getLeft(), nivel + 1, resultado);
+        levelListsHelper(node.getRight(), nivel + 1, resultado);
     }
 }
