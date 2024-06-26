b0deb0769abfbac695a505cffb54ff2f05bfca0a
Sebastian <aviladiego163@gmail.com>
2024-06-26 17:24:40 -0400

    Estructuraas no lineales5

--- src/main/App.java
+++ src/main/App.java
@@ -1,5 +1,6 @@
 package main;
 
+import main.Materia.Controllers.AVLTree;
 import main.Materia.Controllers.ArbolBinario;
 import main.Materia.Models.Node;
 /*
@@ -36,5 +37,11 @@ public class App {
         arbolBinario.postOrderRecursivo(nodeRoot);
         System.out.println("\n InOrder - Recursivo");
         arbolBinario.inOrderRecursivo(nodeRoot);
+
+        AVLTree tree = new AVLTree();
+        int[] values ={10,20,15,24,9,8,21,23,50,25};
+        for (int value:values){
+            tree.insert(value);
+        }
     }
 }
